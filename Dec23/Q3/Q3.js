98f8d24f9a28554979c09580cdf9e1f3afb1cb28
Aman gupta <49397069+Aman99344@users.noreply.github.com>
2021-12-31 10:34:17 +0530

    updated_assignment

--- Dec23/Q3/Q3.js
+++ Dec23/Q3/Q3.js
@@ -2,12 +2,24 @@
 //Write a program to take an input from the user (num) and check //whether it is a prime number or not.
 
 
-var number = prompt("Please enter the number");
+var num = prompt("Please enter the number");
+ 
+ var isPrimeNumber= true;
 
-    for(i = 2; i < num; i = i + 1);
+    for(i = 2; i < num; i = i + 1)
     {
 
-   
+   if(num % i == 0) {
 
+   isPrimeNumber= false;
+     
+    
+ }
+ }
+   if(isPrimeNumber == true){
+       alert((num) + "is a prime");
 
- 
+} else {
+    alert((num) + "is not prime");
+
+}
