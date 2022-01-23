9de9c20eec1949514e6ca39835592c0904ce0482
Aman gupta <49397069+Aman99344@users.noreply.github.com>
2022-01-23 15:24:53 +0530

    submit

---
+++ jan17/index.js
@@ -0,0 +1,14 @@
+//var a = [5,20,6,2];
+ var a = prompt("enter the number");
+
+
+ var result =[];
+
+    for(let i=0; i<a.length; i++){
+    var b =(a[i]+8);
+    var c =(a[i]*2);
+    result.push([b]);
+    console.log([b]);
+    result.push([c]);
+    console.log([c]);
+}
